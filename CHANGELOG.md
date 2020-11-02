d03a55104f00360d29f24f13aa1ee7dff7037cd1
Rob van Groenewoud <git@robvangroenewoud.com>
2020-11-02 23:29:44 +0100

    Added `init` command to initialize the ADR directory
    - Writes directory to `.adr-dir` file
    - Automatically creates directory
    - Generates initial ADR
    - Lists ADRs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,4 +7,5 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 
 ### Added
+- `init` command to initialize ADR directory
 - `list` command to enumerate ADR files
--- cli.js
+++ cli.js
@@ -1,12 +1,21 @@
 #!/usr/bin/env node
 
 const program = require("commander");
+const init = require("./lib/commands/init");
 const list = require("./lib/commands/list");
 const { exit } = require("process");
 
 program.name("adr");
 
 program.command("list").description("list ADRs").action(list);
+
+const commands = [init];
+commands.forEach((command) =>
+  program
+    .command(command.command)
+    .description(command.description)
+    .action(command.action)
+);
 
 program.parseAsync(process.argv).catch((error) => {
   console.log(error);
